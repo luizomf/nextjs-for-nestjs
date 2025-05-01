15ece79c41f4bdd7e1cb20287c3eb3b7326e04f9
Luiz Otávio <luizomf@gmail.com>
2025-05-01 12:05:32 -0300

    Criando o cookie httpOnly com o JWT retornado pelo NestJS

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -1,9 +1,11 @@
 'use server';
 
+import { createLoginSessionFromApi } from '@/lib/login/manage-login';
 import { LoginSchema } from '@/lib/login/schemas';
 import { apiRequest } from '@/utils/api-request';
 import { asyncDelay } from '@/utils/async-delay';
 import { getZodErrorMessages } from '@/utils/get-zod-error-messages';
+import { redirect } from 'next/navigation';
 
 type LoginActionState = {
   email: string;
@@ -60,13 +62,6 @@ export async function loginAction(state: LoginActionState, formData: FormData) {
     };
   }
 
-  console.log(loginResponse.data);
-
-  // await createLoginSession(email);
-  // redirect('/admin/post');
-
-  return {
-    email: formEmail,
-    errors: ['Success'],
-  };
+  await createLoginSessionFromApi(loginResponse.data.accessToken);
+  redirect('/admin/post');
 }
