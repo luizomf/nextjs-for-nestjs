d78c2791f7efbaf3033bed659ee9ab6daaac9e66
Luiz Otávio <luizomf@gmail.com>
2025-05-01 11:45:33 -0300

    Autenticação com cookie e JWT: Migrando Login do Next.js para o NestJS

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -1,13 +1,13 @@
 'use server';
 
-import { createLoginSession } from '@/lib/login/manage-login';
-import { verifyPassword } from '@/lib/login/password-hashing';
+import { LoginSchema } from '@/lib/login/schemas';
+import { apiRequest } from '@/utils/api-request';
 import { asyncDelay } from '@/utils/async-delay';
-import { redirect } from 'next/navigation';
+import { getZodErrorMessages } from '@/utils/get-zod-error-messages';
 
 type LoginActionState = {
-  username: string;
-  error: string;
+  email: string;
+  errors: string[];
 };
 
 export async function loginAction(state: LoginActionState, formData: FormData) {
@@ -15,8 +15,8 @@ export async function loginAction(state: LoginActionState, formData: FormData) {
 
   if (!allowLogin) {
     return {
-      username: '',
-      error: 'Login not allowed',
+      email: '',
+      errors: ['Login not allowed'],
     };
   }
 
@@ -24,36 +24,49 @@ export async function loginAction(state: LoginActionState, formData: FormData) {
 
   if (!(formData instanceof FormData)) {
     return {
-      username: '',
-      error: 'Dados inválidos',
+      email: '',
+      errors: ['Dados inválidos'],
     };
   }
 
-  // Dados que o usuário digitou no form
-  const username = formData.get('username')?.toString().trim() || '';
-  const password = formData.get('password')?.toString().trim() || '';
+  // Validar
+  const formObj = Object.fromEntries(formData.entries());
+  const formEmail = formObj?.email?.toString() || '';
+  const parsedFormData = LoginSchema.safeParse(formObj);
 
-  if (!username || !password) {
+  if (!parsedFormData.success) {
     return {
-      username,
-      error: 'Digite o usuário e a senha',
+      email: formEmail,
+      errors: getZodErrorMessages(parsedFormData.error.format()),
     };
   }
 
-  // Aqui eu checaria se o usuário existe na base de dados
-  const isUsernameValid = username === process.env.LOGIN_USER;
-  const isPasswordValid = await verifyPassword(
-    password,
-    process.env.LOGIN_PASS || '',
+  // Fetch
+  const loginResponse = await apiRequest<{ accessToken: string }>(
+    '/auth/login',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(parsedFormData.data),
+    },
   );
 
-  if (!isUsernameValid || !isPasswordValid) {
+  if (!loginResponse.success) {
     return {
-      username,
-      error: 'Usuário ou senha inválidos',
+      email: formEmail,
+      errors: loginResponse.errors,
     };
   }
 
-  await createLoginSession(username);
-  redirect('/admin/post');
+  console.log(loginResponse.data);
+
+  // await createLoginSession(email);
+  // redirect('/admin/post');
+
+  return {
+    email: formEmail,
+    errors: ['Success'],
+  };
 }
--- src/components/admin/LoginForm/index.tsx
+++ src/components/admin/LoginForm/index.tsx
@@ -11,15 +11,15 @@ import { toast } from 'react-toastify';
 
 export function LoginForm() {
   const initialState = {
-    username: '',
-    error: '',
+    email: '',
+    errors: [],
   };
   const [state, action, isPending] = useActionState(loginAction, initialState);
 
   useEffect(() => {
-    if (state.error) {
+    if (state.errors.length > 0) {
       toast.dismiss();
-      toast.error(state.error);
+      state.errors.forEach(e => toast.error(e));
     }
   }, [state]);
 
@@ -32,12 +32,13 @@ export function LoginForm() {
     >
       <form action={action} className='flex-1 flex flex-col gap-6'>
         <InputText
-          type='text'
-          name='username'
-          labelText='Usuário'
-          placeholder='Seu usuário'
+          type='email'
+          name='email'
+          labelText='E-mail'
+          placeholder='Seu e-mail'
           disabled={isPending}
-          defaultValue={state.username}
+          defaultValue={state.email}
+          required
         />
 
         <InputText
@@ -46,6 +47,7 @@ export function LoginForm() {
           labelText='Senha'
           placeholder='Sua senha'
           disabled={isPending}
+          required
         />
 
         <Button disabled={isPending} type='submit' className='mt-4'>
@@ -56,8 +58,6 @@ export function LoginForm() {
         <p className='text-sm/tight'>
           <Link href='/user/new'>Criar minha conta</Link>
         </p>
-
-        {!!state.error && <p className='text-red-600'>{state.error}</p>}
       </form>
     </div>
   );
