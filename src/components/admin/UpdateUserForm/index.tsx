ec85a6b71a4cbd14a9c61d5439ebb11c8f47408f
Luiz Otávio <luizomf@gmail.com>
2025-05-02 07:01:52 -0300

    Server Action, formulário e componentes para atualização do usuário

--- src/components/admin/UpdateUserForm/index.tsx
+++ src/components/admin/UpdateUserForm/index.tsx
@@ -1,19 +1,31 @@
 'use client';
 
+import { updateUserAction } from '@/actions/user/update-user-action';
 import { Button } from '@/components/Button';
 import { Dialog } from '@/components/Dialog';
 import { InputText } from '@/components/InputText';
+import { PublicUserDto } from '@/lib/user/schemas';
 import { asyncDelay } from '@/utils/async-delay';
 import clsx from 'clsx';
 import { LockKeyholeIcon, OctagonXIcon, UserPenIcon } from 'lucide-react';
 import Link from 'next/link';
-import { useState, useTransition } from 'react';
+import { useActionState, useEffect, useState, useTransition } from 'react';
+import { toast } from 'react-toastify';
 
-export function UpdateUserForm() {
+type UpdateUserFormProps = {
+  user: PublicUserDto;
+};
+
+export function UpdateUserForm({ user }: UpdateUserFormProps) {
+  const [state, action, isPending] = useActionState(updateUserAction, {
+    user,
+    errors: [],
+    success: false,
+  });
   const [isDialogVisible, setIsDialogVisible] = useState(false);
   const [isTransitioning, startTransition] = useTransition();
   const safetyDelay = 10000;
-  const isElementsDisabled = isTransitioning;
+  const isElementsDisabled = isTransitioning || isPending;
 
   function showDeleteAccountDialog(
     e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
@@ -30,6 +42,18 @@ export function UpdateUserForm() {
     //
   }
 
+  useEffect(() => {
+    toast.dismiss();
+
+    if (state.errors.length > 0) {
+      state.errors.forEach(error => toast.error(error));
+    }
+
+    if (state.success) {
+      toast.success('Atualizado com sucesso');
+    }
+  }, [state]);
+
   return (
     <div
       className={clsx(
@@ -37,14 +61,14 @@ export function UpdateUserForm() {
         'text-center max-w-sm mt-16 mb-32 mx-auto',
       )}
     >
-      <form action={''} className='flex-1 flex flex-col gap-6'>
+      <form action={action} className='flex-1 flex flex-col gap-6'>
         <InputText
           type='text'
           name='name'
           labelText='Nome'
           placeholder='Seu nome'
           disabled={isElementsDisabled}
-          defaultValue={''}
+          defaultValue={state.user.name}
         />
 
         <InputText
@@ -53,7 +77,7 @@ export function UpdateUserForm() {
           labelText='E-mail'
           placeholder='Seu e-mail'
           disabled={isElementsDisabled}
-          defaultValue={''}
+          defaultValue={state.user.email}
         />
 
         <div className='flex items-center justify-center mt-4'>
